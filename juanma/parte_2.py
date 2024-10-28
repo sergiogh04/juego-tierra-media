670e710864c370e7a08239232060f1b02a9a9479
Juan Manuel Toro <jtorcab695@fp.iesromerovargas.com>
2024-10-28 11:54:21 +0100

    parte 2

--- juanma/parte_2.py
+++ juanma/parte_2.py
@@ -1,9 +1,11 @@
-# Diccionario para almacenar el equipamiento de los personajes
+from pablo.parte_1 import personajes
+
+# Diccionario para almacenar el equipamiento
 equipos = {}
 
 # Función para añadir un equipamiento
-def añadir_equipamiento():
-    print("----------AÑADIR EQUIPAMIENTO-------------")
+def crear_equipamiento():
+    print("---------- CREAR EQUIPAMIENTO -------------")
     nombre = input("Introduzca el nombre del objeto: ")
 
     if nombre in equipos:
@@ -14,13 +16,13 @@ def añadir_equipamiento():
 
     while True:
         try:
-            potencia = int(input("Introduce la potencia del equipo siendo un numero positivo: "))
+            potencia = int(input("Introduce la potencia del equipo (debe ser un número positivo): "))
             if potencia > 0:
                 break
             else:
                 print("La potencia tiene que ser positiva")
         except ValueError:
-            print("ERROR, el numero no es valido")
+            print("ERROR, el número no es válido")
 
     equipo = {
         "Nombre": nombre,
@@ -35,6 +37,30 @@ def añadir_equipamiento():
     for key, value in equipo.items():
         print(f"{key}: {value}")
 
+# Función para equipar el objeto
+def equipar_objeto():
+    print("---------- EQUIPAMIENTO -------------")
+    nombre_personaje = input("Introduzca el nombre del personaje que quieres equipar: ")
 
-añadir_equipamiento()
+    if nombre_personaje in personajes:
+        personaje = personajes[nombre_personaje]
+        print(f"Se ha encontrado al personaje llamado {nombre_personaje}")
 
+        print("Este es el equipamiento que se puede añadir: ")
+        for nombre, equipo in equipos.items():
+            print(f"Nombre: {equipo['Nombre']}, Tipo: {equipo['Tipo']}, Potencia: {equipo['Potencia']}")
+
+        equipo_personaje = input("Escribe el nombre del equipo que quieres darle al personaje: ")
+
+        if equipo_personaje in equipos:
+            personaje['equipamiento'].append(equipos[equipo_personaje])
+            print(f"Se ha añadido '{equipo_personaje}' a {nombre_personaje}.")
+            print(f"Equipamiento actual de {nombre_personaje}: {personaje['equipamiento']}")
+        else:
+            print(f"El equipamiento '{equipo_personaje}' no existe")
+    else:
+        print(f"El personaje '{nombre_personaje}' no existe")
+
+# Ejemplo de uso
+crear_equipamiento()
+equipar_objeto()
