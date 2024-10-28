22acd99f0445f62ea73db0610a81af5a6826143a
pandgri <pandgri859@fp.iesromerovargas.com>
2024-10-28 09:37:09 +0100

    Registrar Personaje

--- pablo/parte_1.py
+++ pablo/parte_1.py
@@ -1,1 +1,30 @@
-print("Agregar personajes: ")
+
+def registrar_personaje():
+    print("------ REGISTRAR UN NUEVO PERSONAJE ------")
+    nombre = input("Introduzca el nombre del personaje: ")
+    if nombre in personajes:
+        print("Personaje ya existe")
+        return
+
+    raza = input("Razas (Elfo, Humano, Enano, Hobbit): ")
+    faccion = input("Facción del personaje: ")
+    ubicacion = input("Ubicación del personaje: ")
+
+    personaje = {
+        "nombre": nombre,
+        "raza": raza,
+        "faccion": faccion,
+        "ubicacion": ubicacion,
+        "equipamiento": [],
+        "relaciones": [],
+    }
+
+    personajes[nombre] = personaje
+    print(f"Personaje '{personaje["nombre"]}' registrado exitosamente")
+    print(personajes)
+
+
+
+personajes = {}
+registrar_personaje()
+
