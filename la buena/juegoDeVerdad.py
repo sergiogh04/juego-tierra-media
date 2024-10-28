99dbc0b09750e665391e4ac1f29ad054940bb759
pandgri <pandgri859@fp.iesromerovargas.com>
2024-10-28 09:54:08 +0100

    Registrar Personaje

--- la buena/juegoDeVerdad.py
+++ la buena/juegoDeVerdad.py
@@ -1,20 +1,4 @@
 
-def mostrar_menu():
-
-    print("""--- Menú de Gestión de la Tierra Media ---
-    1. Registrar un nuevo personaje
-    2. Añadir equipamiento a un personaje
-    3. Equipar un arma a un personaje
-    4. Establecer relaciones entre personajes
-    5. Mover un personaje a una nueva localización
-    6. Simular una batalla entre dos personajes
-    7. Listar personajes por facción
-    8. Buscar personajes por equipamiento
-    9. Mostrar todos los personajes
-    10. Salir""")
-
-def registrar_personaje():
-    return print("personaje elegido")
 
 def  añadir_equipamiento():
     return print("")
@@ -44,47 +28,83 @@ def mostrar_personajes():
 
 
 def menu_juego():
-
+    personajes = {}
     while True:
         mostrar_menu()
-        try:
-            opcion = int(input("Selecciona una opción: "))
-            match opcion:
+        opcion = int(input("Selecciona una opción: "))
 
-                case 1:
-                    registrar_personaje()
+        match opcion:
+            case 1:
+                registrar_personaje(personajes)
+                print("\n")
+            case 2:
+                añadir_equipamiento()
+                print("\n")
+            case 3:
+                equipar_arma()
+                print("\n")
+            case 4:
+                relaciones_personajes()
+                print("\n")
+            case 5:
+                mover_personaje()
+                print("\n")
+            case 6:
+                simular_batalla()
+                print("\n")
+            case 7:
+                personajes_faccion()
+                print("\n")
+            case 8:
+                buscar_personajes()
+                print("\n")
+            case 9:
+                mostrar_personajes()
+                print("\n")
+            case 10:
+                print("Has salido del programa")
+                break
+            case _:
+                print("Opción no valida")
 
-                case 2:
-                     añadir_equipamiento()
 
-                case 3:
-                     equipar_arma()
+def mostrar_menu():
+    print("""--- Menú de Gestión de la Tierra Media ---
+    1. Registrar un nuevo personaje
+    2. Añadir equipamiento a un personaje
+    3. Equipar un arma a un personaje
+    4. Establecer relaciones entre personajes
+    5. Mover un personaje a una nueva localización
+    6. Simular una batalla entre dos personajes
+    7. Listar personajes por facción
+    8. Buscar personajes por equipamiento
+    9. Mostrar todos los personajes
+    10. Salir""")
 
-                case 4:
-                    relaciones_personajes()
 
-                case 5:
-                    mover_personaje()
+def registrar_personaje(personajes):
+    print("------ REGISTRAR UN NUEVO PERSONAJE ------")
+    nombre = input("Introduzca el nombre del personaje: ")
+    if nombre in personajes:
+        print("Personaje ya existe")
+        return
 
-                case 6:
-                   simular_batalla()
+    raza = input("Razas (Elfo, Humano, Enano, Hobbit): ")
+    faccion = input("Facción del personaje: ")
+    ubicacion = input("Ubicación del personaje: ")
 
-                case 7:
-                   personajes_faccion()
+    personaje = {
+        "nombre": nombre,
+        "raza": raza,
+        "faccion": faccion,
+        "ubicacion": ubicacion,
+        "equipamiento": [],
+        "relaciones": [],
+    }
 
-                case 8:
-                    buscar_personajes()
+    personajes[nombre] = personaje
+    print(f"Personaje '{personaje["nombre"]}' registrado exitosamente")
+    print(personajes)
 
-                case 9:
-                   mostrar_personajes()
-
-                case 10:
-                     print("Has salido del programa")
-                     break
-
-                case _:
-                    print("Opción no valida")
-
-        except ValueError:
-            print("Introduce un numero valido")
-
+if __name__ == "__main__":
+    menu_juego()
