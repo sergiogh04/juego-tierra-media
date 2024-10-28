f2fe81b2cdf160c4c5c6a4414599dcc0ca2528fe
pandgri <pandgri859@fp.iesromerovargas.com>
2024-10-28 11:33:55 +0100

    Listar personajes por facción

--- pablo/parte_1.py
+++ pablo/parte_1.py
@@ -1,5 +1,5 @@
 
-def registrar_personaje():
+def registrar_personaje(personajes):
     print("------ REGISTRAR UN NUEVO PERSONAJE ------")
     nombre = input("Introduzca el nombre del personaje: ")
     if nombre in personajes:
@@ -23,8 +23,47 @@ def registrar_personaje():
     print(f"Personaje '{personaje["nombre"]}' registrado exitosamente")
     print(personajes)
 
+def listar_personajes_faccion(personajes):
+    faccion = input("Introduzca el nombre de una facción: ")
+    personajes_faccion = []
+    for nombre, informacion in personajes.items():
+        if informacion.get("faccion") == faccion:
+            personajes_faccion.append(nombre)
+
+    if personajes_faccion:
+        print(f"Personajes en la facción '{faccion}':")
+        print(personajes_faccion)
+    else:
+        print(f"No se encontraron personajes en la facción '{faccion}'.")
 
 
-personajes = {}
-registrar_personaje()
+
+personajes = {
+    "Aragorn": {
+        "raza": "Humano",
+        "faccion": "La Comunidad del Anillo",
+        "ubicacion": "Rivendel",
+        "equipamiento": [
+            {"nombre": "Andúril", "tipo": "Espada", "potencia": 80}
+        ],
+        "arma_equipada": {"nombre": "Andúril", "tipo": "Espada", "potencia": 80},
+        "relaciones": [
+            {"personaje": "Legolas", "tipo": "Amigo", "nivel_confianza": 10}
+        ]
+    },
+    "Legolas": {
+        "raza": "Elfo",
+        "faccion": "La Comunidad del Anillo",
+        "ubicacion": "Bosque Negro",
+        "equipamiento": [
+            {"nombre": "Arco de Galadriel", "tipo": "Arco", "potencia": 70}
+        ],
+        "arma_equipada": {"nombre": "Arco de Galadriel", "tipo": "Arco", "potencia": 70},
+        "relaciones": [
+            {"personaje": "Aragorn", "tipo": "Amigo", "nivel_confianza": 10}
+        ]
+    }
+}
+
+listar_personajes_faccion(personajes)
 
